6bfb4732e4c79d86656a8ea16651fefcf407facc
Hishamkool <Hishamkool@users.noreply.github.com>
2019-11-25 00:51:22 +0530

    Update java.js

--- TASK 3/js/java.js
+++ TASK 3/js/java.js
@@ -1,64 +1,64 @@
-let running =false;
-let activeplayer=0;
-let cs=[];
-let ts=[];
-const limit=20;
-var flag=0;
+let running = false;
+let activeplayer = 0;
+let cs = [];
+let ts = [];
+const limit = 20;
+var flag = 0;
 
-function GameStart(){
-     var start=document.getElementById("start");
-    
-    if(!running){
-        flag=1;
-        
+function GameStart() {
+    var start = document.getElementById("start");
+
+    if (!running) {
+        flag = 1;
+
         // alert("Game has started");
         start.style.background = 'red ';
-        start.value="STOP";
-        document.getElementById("boxone").style.boxShadow="0px 0px  30px 15px lightgreen";
-        running=true;
+        start.value = "STOP";
+        document.getElementById("boxone").style.boxShadow = "0px 0px  30px 15px lightgreen";
+        running = true;
     }
-    else if(running){
-        flag=0;
+    else if (running) {
+        flag = 0;
         // alert("Game has stopped");
         start.style.background = '#33B30F';
-        start.value="START"; 
-        document.getElementById("boxone").style.boxShadow="";
-        document.getElementById("boxtwo").style.boxShadow="";
-        running=false;
+        start.value = "START";
+        document.getElementById("boxone").style.boxShadow = "";
+        document.getElementById("boxtwo").style.boxShadow = "";
+        running = false;
     }
-    if(flag==0){
-        document.getElementById("Face").src="images/Red-Dice-PNG-Transparent-File.png"
+    if (flag == 0) {
+        document.getElementById("Face").src = "images/Red-Dice-PNG-Transparent-File.png"
     }
 
 }
-function rollOne(){
-    if(flag==1){
-        var df=document.getElementById("Face");
-        df.style.color= 'white';
-        var no=Math.floor((Math.random() *6) +1);
-        
+function rollOne() {
+    if (flag == 1) {
+        var df = document.getElementById("Face");
+        df.style.color = 'white';
+        var no = Math.floor((Math.random() * 6) + 1);
+
         // document.getElementById("boxone").style.boxShadow="0px 0px  30px 15px lightgreen";
-        if(no==1){
-        document.getElementById("Face").src="images/1.png";
-        document.getElementById("Face")
+        if (no == 1) {
+            document.getElementById("Face").src = "images/1.png";
+            document.getElementById("Face")
         }
-        if(no==2){
-            document.getElementById("Face").src="images/2.png";
+        if (no == 2) {
+            document.getElementById("Face").src = "images/2.png";
         }
-        if(no==3){
-            document.getElementById("Face").src="images/3.png";
+        if (no == 3) {
+            document.getElementById("Face").src = "images/3.png";
         }
-        if(no==4){
-            document.getElementById("Face").src="images/4.png";
+        if (no == 4) {
+            document.getElementById("Face").src = "images/4.png";
         }
-        if(no==5){
-            document.getElementById("Face").src="images/5.png";
+        if (no == 5) {
+            document.getElementById("Face").src = "images/5.png";
         }
-        if(no==6){
-            document.getElementById("Face").src="images/6.png";
+        if (no == 6) {
+            document.getElementById("Face").src = "images/6.png";
         }
-       
+
 
     }
-    
+
 }
